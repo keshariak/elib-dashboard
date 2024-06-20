101083fed11ee2220a7cb75e6cf9b9f74287d4f6
keshariak <aryankeshari2003@gmail.com>
2024-06-21 02:06:59 +0530

    add login page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const homepage = () => {
   return (
-    <div>homepage</div>
+    <div className='p-4'>
+        <h1 className='text-xl font-bold'>Home Page </h1>
+       <Button variant={"link"} >  <Link to={"/login"} >login page</Link></Button>
+       
+    </div>
   )
 }
 
